83c6fc38a471a20f417c4e180880daf3f89dbe7b
Karthi Keyan <karthikeyan027@gmail.com>
2021-03-22 17:11:29 +0530

    First commit

---
+++ src/test/java/com/example/rajuspringdemo/RajuspringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rajuspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajuspringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
